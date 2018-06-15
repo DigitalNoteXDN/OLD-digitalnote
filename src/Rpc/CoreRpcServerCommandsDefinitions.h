c2c69950d0762f128ea0ded2c53b4bbaf90d3366
root <root@xdnbuild.cnchains.org>
2018-06-15 22:05:53 +0000

    update

--- src/Rpc/CoreRpcServerCommandsDefinitions.h
+++ src/Rpc/CoreRpcServerCommandsDefinitions.h
@@ -402,6 +402,206 @@ struct BLOCK_HEADER_RESPONSE {
   }
 };
 
+struct f_transaction_short_response {
+  std::string hash;
+  uint64_t fee;
+  uint64_t amount_out;
+  uint64_t size;
+
+  void serialize(ISerializer &s) {
+    KV_MEMBER(hash)
+    KV_MEMBER(fee)
+    KV_MEMBER(amount_out)
+    KV_MEMBER(size)
+  }
+};
+
+struct f_transaction_details_response {
+  std::string hash;
+  size_t size;
+  std::string paymentId;
+  uint64_t mixin;
+  uint64_t fee;
+  uint64_t amount_out;
+
+  void serialize(ISerializer &s) {
+    KV_MEMBER(hash)
+    KV_MEMBER(size)
+    KV_MEMBER(paymentId)
+    KV_MEMBER(mixin)
+    KV_MEMBER(fee)
+    KV_MEMBER(amount_out)
+  }
+};
+
+struct f_block_short_response {
+  uint64_t timestamp;
+  uint32_t height;
+  std::string hash;
+  uint64_t tx_count;
+  uint64_t cumul_size;
+
+  void serialize(ISerializer &s) {
+    KV_MEMBER(timestamp)
+    KV_MEMBER(height)
+    KV_MEMBER(hash)
+    KV_MEMBER(cumul_size)
+    KV_MEMBER(tx_count)
+  }
+};
+
+struct f_block_details_response {
+  uint8_t major_version;
+  uint8_t minor_version;  
+  uint64_t timestamp;
+  std::string prev_hash;
+  uint32_t nonce;
+  bool orphan_status;
+  uint32_t height;
+  uint64_t depth;
+  std::string hash;
+  uint64_t difficulty;
+  uint64_t reward;
+  uint64_t blockSize;
+  size_t sizeMedian;
+  uint64_t effectiveSizeMedian;
+  uint64_t transactionsCumulativeSize;
+  std::string alreadyGeneratedCoins;
+  uint64_t alreadyGeneratedTransactions;
+  uint64_t baseReward;
+  double penalty;
+  uint64_t totalFeeAmount;
+  std::vector<f_transaction_short_response> transactions;
+
+  void serialize(ISerializer &s) {
+    KV_MEMBER(major_version)
+    KV_MEMBER(minor_version)
+    KV_MEMBER(timestamp)
+    KV_MEMBER(prev_hash)
+    KV_MEMBER(nonce)
+    KV_MEMBER(orphan_status)
+    KV_MEMBER(height)
+    KV_MEMBER(depth)
+    KV_MEMBER(hash)
+    KV_MEMBER(difficulty)
+    KV_MEMBER(reward)
+    KV_MEMBER(blockSize)
+    KV_MEMBER(sizeMedian)
+    KV_MEMBER(effectiveSizeMedian)
+    KV_MEMBER(transactionsCumulativeSize)
+    KV_MEMBER(alreadyGeneratedCoins)
+    KV_MEMBER(alreadyGeneratedTransactions)
+    KV_MEMBER(baseReward)
+    KV_MEMBER(penalty)
+    KV_MEMBER(transactions)
+    KV_MEMBER(totalFeeAmount)
+  }
+};
+struct currency_base_coin {
+  std::string name;
+  std::string git;
+
+  void serialize(ISerializer &s) {
+    KV_MEMBER(name)
+    KV_MEMBER(git)
+  }
+};
+
+struct currency_core {
+  std::vector<std::string> SEED_NODES;
+  unsigned EMISSION_SPEED_FACTOR;
+  uint64_t DIFFICULTY_TARGET;
+  size_t CRYPTONOTE_DISPLAY_DECIMAL_POINT;
+  std::string MONEY_SUPPLY;
+  uint64_t DEFAULT_DUST_THRESHOLD;
+  uint64_t MINIMUM_FEE;
+  uint32_t CRYPTONOTE_MINED_MONEY_UNLOCK_WINDOW;
+  size_t CRYPTONOTE_BLOCK_GRANTED_FULL_REWARD_ZONE;
+  size_t CRYPTONOTE_BLOCK_GRANTED_FULL_REWARD_ZONE_V1;
+  size_t CRYPTONOTE_BLOCK_GRANTED_FULL_REWARD_ZONE_V2;
+  uint64_t CRYPTONOTE_PUBLIC_ADDRESS_BASE58_PREFIX;
+  int P2P_DEFAULT_PORT;
+  int RPC_DEFAULT_PORT;
+  size_t MAX_BLOCK_SIZE_INITIAL;
+  uint64_t EXPECTED_NUMBER_OF_BLOCKS_PER_DAY;
+  uint32_t UPGRADE_HEIGHT_V2;
+  uint32_t UPGRADE_HEIGHT_V3;
+  size_t DIFFICULTY_WINDOW;
+  size_t DIFFICULTY_CUT;
+  size_t DIFFICULTY_LAG;
+  uint16_t MIN_MIXIN;
+  uint8_t MANDATORY_MIXIN_BLOCK_VERSION;
+  uint32_t MIXIN_START_HEIGHT;
+  uint32_t MANDATORY_TRANSACTION;
+  uint32_t KILL_HEIGHT;
+  uint64_t TAIL_EMISSION_REWARD;
+  uint8_t CRYPTONOTE_COIN_VERSION;
+  std::string GENESIS_BLOCK_REWARD;
+  size_t DIFFICULTY_WINDOW_V1;
+  size_t DIFFICULTY_WINDOW_V2;
+  size_t DIFFICULTY_CUT_V1;
+  size_t DIFFICULTY_CUT_V2;
+  size_t DIFFICULTY_LAG_V1;
+  size_t DIFFICULTY_LAG_V2;
+  uint32_t ZAWY_DIFFICULTY_BLOCK_INDEX;
+  uint32_t ZAWY_DIFFICULTY_LAST_BLOCK;
+  uint32_t ZAWY_LWMA_DIFFICULTY_BLOCK_INDEX;
+  uint32_t ZAWY_LWMA_DIFFICULTY_LAST_BLOCK;
+  uint32_t ZAWY_LWMA_DIFFICULTY_N;
+  uint32_t BUGGED_ZAWY_DIFFICULTY_BLOCK_INDEX;
+  std::string BYTECOIN_NETWORK;
+  std::string CRYPTONOTE_NAME;
+  std::string GENESIS_COINBASE_TX_HEX;
+  std::vector<std::string> CHECKPOINTS;
+
+  void serialize(ISerializer &s) {
+    KV_MEMBER(SEED_NODES)
+    KV_MEMBER(EMISSION_SPEED_FACTOR)
+    KV_MEMBER(DIFFICULTY_TARGET)
+    KV_MEMBER(CRYPTONOTE_DISPLAY_DECIMAL_POINT)
+    KV_MEMBER(MONEY_SUPPLY)
+    KV_MEMBER(DEFAULT_DUST_THRESHOLD)
+    KV_MEMBER(MINIMUM_FEE)
+    KV_MEMBER(CRYPTONOTE_MINED_MONEY_UNLOCK_WINDOW)
+    KV_MEMBER(CRYPTONOTE_BLOCK_GRANTED_FULL_REWARD_ZONE)
+    KV_MEMBER(CRYPTONOTE_BLOCK_GRANTED_FULL_REWARD_ZONE_V1)
+    KV_MEMBER(CRYPTONOTE_BLOCK_GRANTED_FULL_REWARD_ZONE_V2)
+    KV_MEMBER(CRYPTONOTE_PUBLIC_ADDRESS_BASE58_PREFIX)
+    KV_MEMBER(P2P_DEFAULT_PORT)
+    KV_MEMBER(RPC_DEFAULT_PORT)
+    KV_MEMBER(MAX_BLOCK_SIZE_INITIAL)
+    KV_MEMBER(EXPECTED_NUMBER_OF_BLOCKS_PER_DAY)
+    KV_MEMBER(UPGRADE_HEIGHT_V2)
+    KV_MEMBER(UPGRADE_HEIGHT_V3)
+    KV_MEMBER(DIFFICULTY_WINDOW)
+    KV_MEMBER(DIFFICULTY_CUT)
+    KV_MEMBER(DIFFICULTY_LAG)
+    KV_MEMBER(MIN_MIXIN)
+    KV_MEMBER(MANDATORY_MIXIN_BLOCK_VERSION)
+    KV_MEMBER(MIXIN_START_HEIGHT)
+    KV_MEMBER(MANDATORY_TRANSACTION)
+    KV_MEMBER(KILL_HEIGHT)
+    KV_MEMBER(TAIL_EMISSION_REWARD)
+    KV_MEMBER(CRYPTONOTE_COIN_VERSION)
+    KV_MEMBER(GENESIS_BLOCK_REWARD)
+    KV_MEMBER(DIFFICULTY_WINDOW_V1)
+    KV_MEMBER(DIFFICULTY_WINDOW_V2)
+    KV_MEMBER(DIFFICULTY_CUT_V1)
+    KV_MEMBER(DIFFICULTY_CUT_V2)
+    KV_MEMBER(DIFFICULTY_LAG_V1)
+    KV_MEMBER(DIFFICULTY_LAG_V2)
+    KV_MEMBER(ZAWY_DIFFICULTY_BLOCK_INDEX)
+    KV_MEMBER(ZAWY_DIFFICULTY_LAST_BLOCK)
+    KV_MEMBER(ZAWY_LWMA_DIFFICULTY_BLOCK_INDEX)
+    KV_MEMBER(ZAWY_LWMA_DIFFICULTY_LAST_BLOCK)
+    KV_MEMBER(ZAWY_LWMA_DIFFICULTY_N)
+    KV_MEMBER(BUGGED_ZAWY_DIFFICULTY_BLOCK_INDEX)
+    KV_MEMBER(BYTECOIN_NETWORK)
+    KV_MEMBER(CRYPTONOTE_NAME)
+    KV_MEMBER(GENESIS_COINBASE_TX_HEX)
+    KV_MEMBER(CHECKPOINTS)
+  }
+};
 
 struct COMMAND_RPC_GET_LAST_BLOCK_HEADER {
   typedef EMPTY_STRUCT request;
@@ -430,6 +630,100 @@ struct COMMAND_RPC_GET_BLOCK_HEADER_BY_HEIGHT {
   };
 
   typedef BLOCK_HEADER_RESPONSE response;
+};
+
+struct F_COMMAND_RPC_GET_BLOCKS_LIST {
+  struct request {
+    uint64_t height;
+
+    void serialize(ISerializer &s) {
+      KV_MEMBER(height)
+    }
+  };
+
+  struct response {
+    std::vector<f_block_short_response> blocks; //transactions blobs as hex
+    std::string status;
+
+    void serialize(ISerializer &s) {
+      KV_MEMBER(blocks)
+      KV_MEMBER(status)
+    }
+  };
+};
+
+struct F_COMMAND_RPC_GET_BLOCK_DETAILS {
+  struct request {
+    std::string hash;
+
+    void serialize(ISerializer &s) {
+      KV_MEMBER(hash)
+    }
+  };
+
+  struct response {
+    f_block_details_response block;
+    std::string status;
+
+    void serialize(ISerializer &s) {
+      KV_MEMBER(block)
+      KV_MEMBER(status)
+    }
+  };
+};
+
+struct F_COMMAND_RPC_GET_TRANSACTION_DETAILS {
+  struct request {
+    std::string hash;
+
+    void serialize(ISerializer &s) {
+      KV_MEMBER(hash)
+    }
+  };
+
+  struct response {
+    Transaction tx;
+    f_transaction_details_response txDetails;
+    f_block_short_response block;
+    std::string status;
+
+    void serialize(ISerializer &s) {
+      KV_MEMBER(tx)
+      KV_MEMBER(txDetails)
+      KV_MEMBER(block)
+      KV_MEMBER(status)
+    }
+  };
+};
+
+struct F_COMMAND_RPC_GET_POOL {
+  typedef EMPTY_STRUCT request;
+
+  struct response {
+    std::vector<f_transaction_short_response> transactions; //transactions blobs as hex
+    std::string status;
+
+    void serialize(ISerializer &s) {
+      KV_MEMBER(transactions)
+      KV_MEMBER(status)
+    }
+  };
+};
+struct F_COMMAND_RPC_GET_BLOCKCHAIN_SETTINGS {
+  typedef EMPTY_STRUCT request;
+  struct response {
+    currency_base_coin base_coin;
+    currency_core core;
+    std::vector<std::string> extensions;
+    std::string status;
+
+    void serialize(ISerializer &s) {
+      KV_MEMBER(base_coin)
+      KV_MEMBER(core)
+      KV_MEMBER(extensions)
+      KV_MEMBER(status)
+    }
+  };
 };
 
 struct COMMAND_RPC_QUERY_BLOCKS {
